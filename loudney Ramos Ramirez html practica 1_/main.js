847c0e98aa2f04004a432613b883bbdc3cf2cdbb
loudney <105092036+loudney@users.noreply.github.com>
2022-11-23 13:53:43 -0400

    Add files via upload

---
+++ loudney Ramos Ramirez html practica 1_/main.js
@@ -0,0 +1,16 @@
+/*let a=prompt('Nombre',' ')
+     console.log('hola',a);
+     document.getElementById('nom').innerHTML=a;*/
+/*window.alert("Bienvenido al sitio web");*/
+     
+let mult=prompt('Numero',' ')
+i=1
+res='';
+while(i<=mult)    
+{
+     res=res+(i*5)+'-';
+     i=i+1;
+     
+}
+
+console.log (res)
